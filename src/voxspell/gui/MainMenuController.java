7a8a91b3967ad131d8e2585fc4805031f9866594
Kevin Hira <khir664@aucklanduni.ac.nz>
2016-09-25 23:46:08 +1300

    Added buttons, labels to GUI for main menu. Added exit button logic. > src/voxspell/gui/MainMenu.fxml:     Added design for some of the buttons.     Added labels for title and author. > src/voxspell/gui/MainMenuController.java:     Added logic for the exit button.

--- src/voxspell/gui/MainMenuController.java
+++ src/voxspell/gui/MainMenuController.java
@@ -6,12 +6,32 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.application.Platform;
+
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class MainMenuController implements Initializable {
 
     @FXML
+    private Label lblAuthor;
+
+    @FXML
+    private Button btnNewTest;
+
+    @FXML
+    private Button btnReviewTest;
+
+    @FXML
     private Label lblTitle;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    void btnExitPressed(ActionEvent event) {
+        Platform.exit();
+    }
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
