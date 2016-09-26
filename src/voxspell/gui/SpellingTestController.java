5b0471f15d8097dbf47e0f49eb3d45a488df371a
Kevin Hira <khir664@aucklanduni.ac.nz>
2016-09-26 22:05:16 +1300

    Add to skeleton for new test GUI.
    > src/voxspell/gui/SpellingTest.fxml:
        * Added more components, set up injection fx:ids.
    > src/voxspell/gui/SpellingTestController.java:
        * As above.
        * Experimented with progress bars/indicators.

--- src/voxspell/gui/SpellingTestController.java
+++ src/voxspell/gui/SpellingTestController.java
@@ -9,12 +9,57 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.TextFlow;
+import javafx.scene.text.Text;
+
 
 
 public class SpellingTestController implements Initializable {
 
     @FXML
     private Button btnMenu;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblLevel;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private Button btnReplay;
+
+    @FXML
+    private TextFlow tflResults;
+
+    @FXML
+    private ProgressIndicator pgiWheel;
+
+    @FXML
+    private ProgressBar pgbProgress;
+
+    @FXML
+    private TextField txtResponse;
+
+    private int _currentWord = 0;
+
+    @FXML
+    void btnSubmitPressed(ActionEvent event) {
+        if (!txtResponse.getText().equals("") && _currentWord < 10) {
+            ++_currentWord;
+            pgiWheel.setProgress(_currentWord / 10.0);
+            pgbProgress.setProgress(_currentWord / 10.0);
+
+            Text t = new Text(_currentWord + " " + txtResponse.getText() + "\n");
+            tflResults.getChildren().add(t);
+        }
+    }
 
     @FXML
     void btnMenuPressed(ActionEvent event) {
